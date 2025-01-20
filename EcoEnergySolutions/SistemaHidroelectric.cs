f89ac8782127ecd3fbe5cfb8a863b8f8c8aa64fc
Laiacastle <laia.castillo.7e6@itb.cat>
2025-01-20 16:35:34 +0100

    Creades les clases. Tanca #2

--- EcoEnergySolutions/SistemaHidroelectric.cs
+++ EcoEnergySolutions/SistemaHidroelectric.cs
@@ -7,7 +7,7 @@ namespace EcoEnergySolutions
 {
     public class SistemaHidroelectric: SistemaEnergia, ICalculEnergia
     {
-        public double CabalAigua { get; set; }
+        public double CabalAigua { get; set;}
         public double CalcEnergia() =>this.CabalAigua * 9.8 * 0.8;
         public void MostraInforme() => Console.WriteLine($"data: {this.Date} tipus: {this.Type} cabal d'aigua: {this.CabalAigua}");
         public bool ConfParametre() => this.CabalAigua >= 20.0;
